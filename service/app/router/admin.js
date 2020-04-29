96b06384ee8ae2d94ebb41b438ac5f2c2c60f2ba
dengxiaoping <690566208@qq.com>
2020-04-29 16:55:33 +0800

    接口增加删除和修改

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -4,4 +4,9 @@ module.exports = app =>{
     router.get('/admin/index',adminauth ,controller.admin.main.index)
     router.post('/admin/checkOpenId',controller.admin.main.checkLogin)
     router.get('/admin/getTypeInfo',controller.admin.main.getTypeInfo)
+    router.post('/admin/addArticle',adminauth,controller.admin.main.addArticle)
+    router.post('/admin/updateArticle',adminauth,controller.admin.main.updateArticle)
+    router.get('/admin/getArticleList',adminauth,controller.admin.main.getArticleList)
+    router.get('/admin/delArticle/:id',adminauth,controller.admin.main.delArticle)
+    router.get('/admin/getArticleById/:id',adminauth,controller.admin.main.getArticleById)
 }
